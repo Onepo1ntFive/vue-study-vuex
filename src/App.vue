a9739d53d7f0ccbe763786801814c9438b7d2b58
Sergey Solovyev <html@laserwar.com>
2021-05-14 17:04:21 +0300

    commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,44 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div id="app">
+        <h1>{{ postsCount }}</h1>
+        <div class="post" v-for="post in allPosts" :key="post.id">
+            <h2>{{ post.title }}</h2>
+            <p>{{ post.body }}</p>
+        </div>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import { mapGetters, mapActions } from "vuex";
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+    name: "App",
+    computed: mapGetters(["allPosts", "postsCount"]),
+    methods: mapActions(["fetchPosts"]),
+
+    async mounted() {
+        this.fetchPosts( );
+    },
+    components: {},
+};
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    color: #2c3e50;
+}
+.post {
+    max-width: 500px;
+    margin: 0 auto 20px;
+    padding-bottom: 20px;
+    border-top: 1px solid #ccc;
+}
+.post p:last-child {
+    margin-bottom: 0;
+}
+h1 {
+    text-align: center;
 }
 </style>
